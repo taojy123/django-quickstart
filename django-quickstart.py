da061bcc7c621f59ebd83cbaee4daff1d65ad094
taojy123 <taojy123@163.com>
2016-11-24 16:51:42 +0800

    improve admin

--- django-quickstart.py
+++ django-quickstart.py
@@ -189,20 +189,50 @@ open(pname + "/models.py", "w").write(outstr)
 
 #create admin
 outstr = """
+from collections import OrderedDict
+
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.sites.models import Site
+from django.utils.text import capfirst
 
 from models import *
+
+
+def find_model_index(name):
+    count = 0
+    print admin.site._registry
+    for model, model_admin in admin.site._registry.items():
+        if capfirst(model._meta.verbose_name_plural) == name:
+            return count
+        else:
+            count += 1
+    return count
+
+
+def index_decorator(func):
+    def inner(*args, **kwargs):
+        templateresponse = func(*args, **kwargs)
+        for app in templateresponse.context_data['app_list']:
+            app['models'].sort(key=lambda x: find_model_index(x['name']))
+        return templateresponse
+    return inner
+
+
+registry = OrderedDict()
+registry.update(admin.site._registry)
+admin.site._registry = registry
+admin.site.index = index_decorator(admin.site.index)
+admin.site.app_index = index_decorator(admin.site.app_index)
+
+admin.site.unregister(Group)
+admin.site.unregister(Site)
 
 
 @admin.register(Reminder)
 class ReminderAdmin(admin.ModelAdmin):
     list_display = ['id', '__str__']
 
-
-admin.site.unregister(Group)
-admin.site.unregister(Site)
 """
 open(pname + "/admin.py", "w").write(outstr)
 
