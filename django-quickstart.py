4193e70231657e92034a252445b3ae4da69ff688
taojy123 <taojy123@163.com>
2017-08-01 17:53:47 +0800

    fix

--- django-quickstart.py
+++ django-quickstart.py
@@ -546,8 +546,8 @@ open("run.bat", "w").write("""
 set PATH=%PATH%;C:\Python27;C:\Python27\Scripts;
 pip install -r requirement.txt
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
 explorer http://127.0.0.1:8000/
+python manage.py runserver 0.0.0.0:8000
 """)
 
 
